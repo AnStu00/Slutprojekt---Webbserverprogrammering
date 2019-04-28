5194401a494001c8e01eab397a08c4d2e5ff4eb3
André Sturesson <multispelare@gmail.com>
2019-04-28 19:06:03 +0200

    PDO uppdatering för forumet
    Nu börjat bitarna falla på plats med forumet, efter mycket om och men. Jag började med att göra koden lite säkrare samt skrev jag om all php till PDO för att följa trenden på sidan. Vill även ha säkerheten på max.
    
    Har gjort varje kategori som en knapp, om man klickar på knappen kommer man vidare till ämne.php med en id som skickas med get i URL:n. Detta är för att koden ska veta vilka trådar den ska visa.
    
    Detta gör att jag slipper göra en ny sida för varje kategori. Nu spelar det ingen roll hur många kategorier som finns, den kommer alltid visa rätt kategori med rätt trådar i.
    
    Jag vet att det finns vissa säkerhetsbrister med en ID i urln, kan göras SQL injektioner. Jag hoppas kunna skicka iväg det med POST senare, genom att göra ett formulär men för principens skull gör jag på detta vis tills vidare.
    
    Ska försöka hinna klart med forumet idag.

--- forum.php
+++ forum.php
@@ -2,116 +2,77 @@
 $sidtitel = "Forum";
 include 'header.php';
 include 'nav.php';
-
-if(isset($_POST[])){
-
-
-
-}
-
-
-
-$query = "SELECT * FROM kategorier";
-$query1 = "SELECT * FROM ämne";
-$query2 = "SELECT * FROM posts";
 ?>
 <!-- Första Sektionen -->
 <section class="page-top-section set-bg" data-setbg="img/topp-på-sida.jpg">
-    <div class="container">
-            <h2>Forum</h2>
-            <div class="site-breadcrumb">
-                <a href="index.php">Hem</a> / <span>Forum</span>
-                    </div>
-            </div>
+  <div class="container">
+    <h2>Forum
+    </h2>
+    <div class="site-breadcrumb">
+      <a href="index.php">Hem
+      </a> /
+      <span>Forum
+      </span>
+    </div>
+  </div>
 </section>
-<h2>Här kommer forumet!</h2>
-
-<a class="item" href="admin/skapa-ämne.php">Skapa ett ämne</a> -
-<a class="item" href="admin/skapa-kategori.php">Skapa en kategori</a>
-<div class="table-responsive-sm">
-  <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Kategorier</th>
-      <th scope="col">Beskrivning</th>
-      <th scope="col">KategoriID</th>
-    </tr>
-  </thead>
-  <tbody>
-  <form method="post" action="forum.php">
+<h2>Här kommer forumet!
+</h2>
 <?php
-foreach ($db->query($query) as $row){
-  echo '<tr>';
-echo '<td>';
-?>
-<input type="submit" class="btn registreraknapp" name="<?php echo $row['kat_id']; ?>" value=<?php echo $row['kat_namn'];?>>
-<?php
-echo '</form>';
-echo '</td>';
-echo '<td>';
-echo $row['kat_beskrivning'];
-echo '</td>';
-echo '<td>';
-echo $row['kat_id'];
-echo '</td>';
-echo '</tr>';
+$query = "SELECT kat_id, kat_namn, kat_beskrivning FROM kategorier";
+try{
+  $result = $db->prepare($query);
+  $result->execute();
+  $rows = $result->fetchAll();
+  $result1 = $db->prepare($query);
+  $result1->execute();
+  $row_count = $result1->fetchAll(PDO::FETCH_NUM);
 }
-
-
+catch(PDOException $ex){
+  die("Kunde inte köra queryn: " . $ex->getMessage());
+}
+if(!$result)
+{
+  echo 'Kategorierna kunde inte visas, Testa igen en annan gång :).';
+}
+else
+{
+  if($row_count == 0)
+  {
+    echo 'Finns inga kategorier än.';
+  }
+else
+{
+//Tar fram tabellen
 ?>
-  </tbody>
+<table class="table table-striped">
   <thead>
     <tr>
-      <th scope="col">Tråd</th>
-      <th scope="col">Skapad av</th>
-      <th scope="col">Passande Kategori ID</th>
+      <th scope="col">Kategorier
+      </th>
+      <th scope="col">Kategori Beskrivning
+      </th>
     </tr>
   </thead>
   <tbody>
-    <?php
-    foreach ($db->query($query1) as $row1){
-      echo '<tr>';
-    echo '<td>';
-    echo $row1['topic_subject'];
-    echo '</td>';
-    echo '<td>';
-    echo $row1['topic_av'];
-    echo '</td>';
-    echo '<td>';
-    echo $row1['topic_kat'];
-    echo '</td>';
-    echo '</tr>';
-    }
-    ?>
-  </tbody>
-  <thead>
+    <?php foreach($rows as $row): ?>
     <tr>
-      <th scope="col">Innehåll i tråden</th>
-      <th scope="col">Tid när innehållet skrev</th>
-      <th scope="col">Skapad Av</th>
+      <td>
+        <a href="ämne.php?id=<?php echo $row['kat_id'];?>"class="btn registreraknapp">
+          <?php echo htmlentities($row['kat_namn'], ENT_QUOTES, 'UTF-8'); ?>
+        </a>
+      </td>
+      <td>
+        <?php echo htmlentities($row['kat_beskrivning'], ENT_QUOTES, 'UTF-8'); ?>
+      </td>
     </tr>
-  </thead>
-  <tbody>
-    <?php
-    foreach ($db->query($query2) as $row2){
-      echo '<tr>';
-    echo '<td>';
-    echo $row2['post_innehåll'];
-    echo '</td>';
-    echo '<td>';
-    echo $row2['post_datum'];
-    echo '</td>';
-    echo '<td>';
-    echo $row2['post_av'];
-    echo '</td>';
-    echo '</tr>';
-    }
-    ?>
+    <?php endforeach; ?>
   </tbody>
-  </table>
-</div>
-
-<!-- Första Sektionens slut -->
+</table>
+<?php
+  }
+}
+?>
 <?php
 include 'footer.php';
 ?>
